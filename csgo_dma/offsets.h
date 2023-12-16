cf2b7421843c5973e6422203a71d05df0889115a
AtomicBottle <84610772+atombottle@users.noreply.github.com>
2023-12-16 21:05:55 +0800

    Update offsets.h

--- csgo_dma/offsets.h
+++ csgo_dma/offsets.h
@@ -1,16 +1,3 @@
-//credits: https://github.com/frk1/hazedumper/blob/master/csgo.hpp
+#define m_iHealth 0x32c //int:812
+#define LocalPawn 0x16C8ED8
 
-#include <cstdint>
-
-namespace offset{
-     //int format,because there's some error while using player_index/localplayer_ptr to r memory
-    
-    constexpr ::std::ptrdiff_t m_iHealth = 256;
-    constexpr ::std::ptrdiff_t m_iTeamNum = 244;
-    constexpr ::std::ptrdiff_t m_iCrosshairId = 71736;
-
-   
-    constexpr ::std::ptrdiff_t dwEntityList = 0x4DFFF7C;
-    constexpr ::std::ptrdiff_t dwForceAttack = 52616680;
-    constexpr ::std::ptrdiff_t dwLocalPlayer = 0xDEA98C;
-}
